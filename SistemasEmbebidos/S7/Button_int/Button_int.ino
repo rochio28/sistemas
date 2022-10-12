6cb3b53a6e03b4028e1fa671132ba807cc60359a
rochio28 <rocio.vcordero@ciencias.unam.mx>
2022-10-12 17:40:34 -0500

    S7

---
+++ SistemasEmbebidos/S7/Button_int/Button_int.ino
@@ -0,0 +1,36 @@
+struct Button {
+  const int PIN;
+  int numTimesButtonPressed;
+  bool pressed;
+  bool bStatus;
+};
+
+Button button1 = {33, 0, false};
+
+void IRAM_ATTR isr_button(){
+  // ---------------
+  button1.numTimesButtonPressed += 1;   // button1.numTimesButtonPressed = button1.numTimesButtonPressed + 1;
+  button1.pressed = true;
+  button1.bStatus ^= 1;                 // button1.bStatus = button1.bStatus ^ 1;
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(button1.PIN, INPUT);
+  attachInterrupt(button1.PIN, isr_button, RISING);
+}
+
+void loop() {
+
+    if(button1.bStatus == true){
+    //if(button1.pressed == true){  
+    Serial.printf("El botón se ha presionado %u veces \n", button1.numTimesButtonPressed);
+    button1.pressed = false;
+    } else{
+    Serial.print("bStatus: ");
+    Serial.println(button1.bStatus);
+    }
+
+  delay(170);
+
+}
